e7163e6fe6cccb7df41647b1912b4d4c601536ba
Kurt Renzo Acosta <18444033+kuuuurt@users.noreply.github.com>
2021-07-18 11:22:49 +0800

    Uses loading state in android

--- androidApp/src/main/java/com/kurt/jokes/presentation/screens/JokesScreen.kt
+++ androidApp/src/main/java/com/kurt/jokes/presentation/screens/JokesScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -11,21 +12,33 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.kurt.jokes.mobile.domain.entities.Joke
 import com.kurt.jokes.mobile.presentation.features.jokes.JokesViewModel
+import com.kurt.jokes.mobile.presentation.models.UiState
 import com.kurt.jokes.presentation.MainViewModelFactory
 
 @Composable
 fun JokesScreen() {
     val viewModel = viewModel<JokesViewModel>(factory = MainViewModelFactory())
     val jokes = viewModel.jokes.collectAsState(listOf())
+    val getJokesState = viewModel.jokesState.collectAsState(UiState.Loading)
 
-    LazyColumn(modifier = Modifier.fillMaxSize()) {
-        items(jokes.value) {
-            JokeItem(it)
+    if (getJokesState.value == UiState.Loading) {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            CircularProgressIndicator()
+        }
+    } else {
+        LazyColumn(modifier = Modifier.fillMaxSize()) {
+            items(jokes.value) {
+                JokeItem(it)
+            }
         }
     }
 }
